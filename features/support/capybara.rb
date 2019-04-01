4a0b21d4aab5a6e04c64b1bef782fcfa29c3d801
Jorge Espinar <jor0veg@gmail.com>
2019-04-01 16:31:15 +0200

    Update capybara.rb

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -4,3 +4,14 @@ Capybara.default_driver = :selenium
 Capybara.default_driver = :selenium_chrome
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
+
+require 'rspec/retry'
+
+RSpec.configure do |config|
+  # show retry status in spec process
+  config.verbose_retry = true
+  # Try twice (retry once)
+  config.default_retry_count = 2
+  # Only retry when Selenium raises Net::ReadTimeout
+  config.exceptions_to_retry = [Net::ReadTimeout]
+end
